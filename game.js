b53e99b351decb84fca622f4739853ebcf582ef3
Soumik Nandi <soumikn@amazon.com>
2021-12-08 16:44:58 +0530

    Added start button for mobile

--- game.js
+++ game.js
@@ -7,15 +7,8 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).touchstart(function () {
-  if (!started) {
-    $("#level-title").text("Level " + level);
-    nextSequence();
-    started = true;
-  }
-});
 
-$(document).keypress(function() {
+$("#start").click(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
